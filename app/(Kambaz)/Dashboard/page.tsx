5d94a179e117ae8e55a6226b39c7009f7221a941
Hetarth Dave <davehetarth@gmail.com>
2025-10-06 20:58:54 -0400

    did last minute changes

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+
 import Image from "next/image";
+
 import { Row, Col } from "react-bootstrap";
+
 import { Card, CardImg, CardText, CardTitle, CardBody } from "react-bootstrap";
+
 import { Button } from "react-bootstrap";
+
 import { LuNotebookPen } from "react-icons/lu";
+
 import { BsThreeDotsVertical } from "react-icons/bs";
 
 export default function Dashboard() {
@@ -11,266 +17,219 @@ export default function Dashboard() {
     <div id="wd-dashboard" className="ms-3">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
-      <div id="wd-dashboard-courses" className="d-flex flex-wrap gap-4">
-        <Row xs={1} md={5} className="g-4 mb-4">
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card className="position-relative">
+      <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4 me-3">
+        {/* Card 1 */}
+        <Col>
+          <Card className="h-100">
+            <div className="position-relative">
+              <CardImg
+                variant="top"
+                src="/images/reactjs.jpg"
+                style={{ height: "160px", objectFit: "cover" }}
+              />
               <BsThreeDotsVertical
                 className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
+                style={{ top: "10px", right: "10px", cursor: "pointer" }}
               />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1234 React Js
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            {" "}
-            <Card>
+            </div>
+            <Link
+              href="/Courses/CS1234/Home"
+              className="text-decoration-none text-dark d-flex flex-column flex-grow-1"
+            >
+              <CardBody className="d-flex flex-column">
+                <CardTitle className="text-truncate">CS1234 React Js</CardTitle>
+                <CardText>Full Stack software</CardText>
+                <div className="mt-auto">
+                  {" "}
+                  <LuNotebookPen className="fs-4" />{" "}
+                </div>
+              </CardBody>
+            </Link>
+          </Card>
+        </Col>
+
+        {/* Card 2 */}
+        <Col>
+          <Card className="h-100">
+            <div className="position-relative">
+              <CardImg
+                variant="top"
+                src="/images/Cloud-Computing.jpg"
+                style={{ height: "160px", objectFit: "cover" }}
+              />
               <BsThreeDotsVertical
                 className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
+                style={{ top: "10px", right: "10px", cursor: "pointer" }}
               />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1235 React JS
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>{" "}
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            {" "}
-            <Card>
+            </div>
+            <Link
+              href="/Courses/CS1235/Home"
+              className="text-decoration-none text-dark d-flex flex-column flex-grow-1"
+            >
+              <CardBody className="d-flex flex-column">
+                <CardTitle className="text-truncate">
+                  CS1235 Cloud Computing
+                </CardTitle>
+                <CardText>Learn Cloud Computing</CardText>
+                <div className="mt-auto">
+                  {" "}
+                  <LuNotebookPen className="fs-4" />{" "}
+                </div>
+              </CardBody>
+            </Link>
+          </Card>
+        </Col>
+
+        {/* Card 3 */}
+        <Col>
+          <Card className="h-100">
+            <div className="position-relative">
+              <CardImg
+                variant="top"
+                src="/images/Data-Analytics.jpg"
+                style={{ height: "160px", objectFit: "cover" }}
+              />
               <BsThreeDotsVertical
                 className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
+                style={{ top: "10px", right: "10px", cursor: "pointer" }}
               />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1232 React JS
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>{" "}
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            {" "}
-            <Card>
+            </div>
+            <Link
+              href="/Courses/CS1232/Home"
+              className="text-decoration-none text-dark d-flex flex-column flex-grow-1"
+            >
+              <CardBody className="d-flex flex-column">
+                <CardTitle className="text-truncate">
+                  CS1232 Data Analytics
+                </CardTitle>
+                <CardText>Data Analytics</CardText>
+                <div className="mt-auto">
+                  {" "}
+                  <LuNotebookPen className="fs-4" />{" "}
+                </div>
+              </CardBody>
+            </Link>
+          </Card>
+        </Col>
+
+        {/* Card 4 */}
+        <Col>
+          <Card className="h-100">
+            <div className="position-relative">
+              <CardImg
+                variant="top"
+                src="/images/Machine-learning.jpg"
+                style={{ height: "160px", objectFit: "cover" }}
+              />
               <BsThreeDotsVertical
                 className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
+                style={{ top: "10px", right: "10px", cursor: "pointer" }}
               />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1236 React JS
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>{" "}
-          </Col>
-        </Row>
-        <Row xs={1} md={5} className="g-4 mb-4">
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            <Card>
+            </div>
+            <Link
+              href="/Courses/CS1232/Home"
+              className="text-decoration-none text-dark d-flex flex-column flex-grow-1"
+            >
+              <CardBody className="d-flex flex-column">
+                <CardTitle className="text-truncate">
+                  CS1232 Machine Learning
+                </CardTitle>
+                <CardText>Machine Learning</CardText>
+                <div className="mt-auto">
+                  {" "}
+                  <LuNotebookPen className="fs-4" />{" "}
+                </div>
+              </CardBody>
+            </Link>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="h-100">
+            <div className="position-relative">
+              <CardImg
+                variant="top"
+                src="/images/DBMS.jpg"
+                style={{ height: "160px", objectFit: "cover" }}
+              />
               <BsThreeDotsVertical
                 className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
+                style={{ top: "10px", right: "10px", cursor: "pointer" }}
               />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1237 React JS
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            {" "}
-            <Card>
+            </div>
+            <Link
+              href="/Courses/CS1232/Home"
+              className="text-decoration-none text-dark d-flex flex-column flex-grow-1"
+            >
+              <CardBody className="d-flex flex-column">
+                <CardTitle className="text-truncate">CS1232 DBMS</CardTitle>
+                <CardText>Learn DBMS</CardText>
+                <div className="mt-auto">
+                  {" "}
+                  <LuNotebookPen className="fs-4" />{" "}
+                </div>
+              </CardBody>
+            </Link>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="h-100">
+            <div className="position-relative">
+              <CardImg
+                variant="top"
+                src="/images/Algo.jpg"
+                style={{ height: "160px", objectFit: "cover" }}
+              />
               <BsThreeDotsVertical
                 className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
+                style={{ top: "10px", right: "10px", cursor: "pointer" }}
               />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1231 React JS
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>{" "}
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            {" "}
-            <Card>
+            </div>
+            <Link
+              href="/Courses/CS1232/Home"
+              className="text-decoration-none text-dark d-flex flex-column flex-grow-1"
+            >
+              <CardBody className="d-flex flex-column">
+                <CardTitle className="text-truncate">
+                  CS1232 Algorithms
+                </CardTitle>
+                <CardText>Learn Algorithms</CardText>
+                <div className="mt-auto">
+                  {" "}
+                  <LuNotebookPen className="fs-4" />{" "}
+                </div>
+              </CardBody>
+            </Link>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="h-100">
+            <div className="position-relative">
+              <CardImg
+                variant="top"
+                src="/images/Networks.jpg"
+                style={{ height: "160px", objectFit: "cover" }}
+              />
               <BsThreeDotsVertical
                 className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
+                style={{ top: "10px", right: "10px", cursor: "pointer" }}
               />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1239 React JS
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>{" "}
-          </Col>
-          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-            {" "}
-            <Card>
-              <BsThreeDotsVertical
-                className="position-absolute fs-4 text-white"
-                style={{ top: "10px", right: "6px" }}
-              />
-              <Link
-                href="/Courses/1234/Home"
-                className="wd-dashboard-course-link text-decoration-none text-dark"
-              >
-                <CardImg
-                  variant="top"
-                  src="/images/reactjs.jpg"
-                  width="100%"
-                  height={160}
-                />
-                <CardBody>
-                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    CS1238 React JS
-                  </CardTitle>
-                  <CardText
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "75px" }}
-                  >
-                    Full Stack software developer
-                  </CardText>
-                  <LuNotebookPen className="fs-4" />
-                </CardBody>
-              </Link>
-            </Card>{" "}
-          </Col>
-        </Row>
-      </div>
+            </div>
+            <Link
+              href="/Courses/CS1232/Home"
+              className="text-decoration-none text-dark d-flex flex-column flex-grow-1"
+            >
+              <CardBody className="d-flex flex-column">
+                <CardTitle className="text-truncate">CS1232 Networks</CardTitle>
+                <CardText>Computer Networks</CardText>
+                <div className="mt-auto">
+                  {" "}
+                  <LuNotebookPen className="fs-4" />{" "}
+                </div>
+              </CardBody>
+            </Link>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
